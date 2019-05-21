8544fef68217f0e7774216176bc15b420f4b1aea
Rakesh Misra <rakeshmisra@stanford.edu>
2019-05-21 15:37:10 -0700

    updated bucket

--- js/script_all.js
+++ js/script_all.js
@@ -17,7 +17,7 @@ var enable_video = true;
 var enable_audio = false;
 var database = firebase.database().ref();
 var storageRef = firebase.storage().ref();
-var storage = firebase.app().storage("gs://netwebrtc");
+var storage = firebase.app().storage("gs://netwebrtc.appspot.com");
 var storageRef = storage.ref();
 
 //var friendsVideo = document.getElementById("friendsVideo");
